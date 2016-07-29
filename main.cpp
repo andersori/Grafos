9adf125f84aa60f893fe53c9b8af5f5380d7c981
Anderson <andersori7@gmail.com>
2016-07-28 21:13:17 -0300

    Caminho minimo finalizado!
    O algoritmo utilizado para detectar o caminho minimo é o algoritmo de Dijkstra.

--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
 #include <vector>
 #include <unordered_map>
 #include <map>
-#include <typeinfo>
 #include <queue>
 #include <unordered_set>
 
@@ -33,11 +32,7 @@ struct Matriz_Custo{
     inline const vector<Custo>& visinhos(Localizacao id){
         return arestas[id];
     }
-    const Custo custo(Localizacao pai, Localizacao adj){
-        return arestas[pai][adj];
-    }
 };
-
 
 template<typename Grafo>
 void busca_em_largura(Grafo grafo, typename Grafo::Localizacao inicio){
@@ -64,51 +59,76 @@ void busca_em_largura(Grafo grafo, typename Grafo::Localizacao inicio){
 }
 
 template<typename Grafo, typename Matriz>
-void caminho_minimo(Grafo grafo, Matriz matriz_custo, typename Grafo::Localizacao inicio, typename Grafo::Localizacao destino){
+void dijkstra(Grafo grafo, Matriz matriz_custo, typename Grafo::Localizacao inicio, typename Grafo::Localizacao destino){
+    if(!grafo.arestas.count(inicio) || !grafo.arestas.count(destino)){
+        cout<<"Algumas ou ambas as vertices nao pertencem ao grafo"<<endl;
+        return;
+    }
+
     typedef typename Grafo::Localizacao Localizacao;
+    typedef typename Matriz::Custo Custo;
 
-    priority_queue<pair<unsigned int, Localizacao>,  vector<pair<unsigned int, Localizacao> >, greater<pair<unsigned int, Localizacao> > > verifique;
-    unordered_set<Localizacao> visitados;
+    priority_queue<pair<Custo, Localizacao>,  vector<pair<Custo, Localizacao> >, greater<pair<Custo, Localizacao> > > visitados;
+    unordered_set<Localizacao> verificados;
 
-    verifique.push(make_pair(0,inicio));
-    visitados.insert(inicio);
+    visitados.push(make_pair(0,inicio));
+    verificados.insert(inicio);
 
-    unsigned int* distancia = new unsigned int[grafo.arestas.size()];
-    Localizacao* antecessor = new Localizacao[grafo.arestas.size()];
+    map<Localizacao, Custo> distancia;
+    map<Localizacao, Localizacao> antecessor;
 
-    for(unsigned int i = 0; i < grafo.arestas.size(); i++){
-        distancia[i] = INFINITO;
-        antecessor[i] = inicio;
+    typename map<Localizacao, vector<Localizacao> >::iterator it;
+
+    for(it = grafo.arestas.begin(); it != grafo.arestas.end(); it++){
+        //cout<<(*it).first<<endl;
+        distancia[(*it).first] = INFINITO;
+        antecessor[(*it).first] = inicio;
     }
 
     distancia[inicio] = 0;
 
-    while(!verifique.empty()){
-        Localizacao vertice_menor_custo = verifique.top().second;
-        verifique.pop();
-        visitados.insert(vertice_menor_custo);
+    while(!visitados.empty() && !verificados.count(destino)){
+        Localizacao vertice_menor_custo = visitados.top().second;
+        visitados.pop();
 
         if(vertice_menor_custo == destino){
             break;
         }
 
         vector<Localizacao> visinhos = grafo.visinhos(vertice_menor_custo);
+
         typename Grafo::iterador it;
 
+        unsigned int i = 0;
         for(it = visinhos.begin(); it != visinhos.end(); it++){
-            if(!visitados.count(vertice_menor_custo)){
-                unsigned int dist = distancia[vertice_menor_custo] + matriz_custo.custo(vertice_menor_custo, *it);
+
+            if(!verificados.count(*it)){
+                unsigned int dist = distancia[vertice_menor_custo] + matriz_custo.arestas[vertice_menor_custo][i++];
                 if(distancia[*it] > dist){
                     distancia[*it] = dist;
                     antecessor[*it] = vertice_menor_custo;
                 }
+                visitados.push(make_pair(dist,*it));
+                verificados.insert(*it);
             }
         }
     }
+    system("pause");
 
-    cout<<"Distancia do caminho minimo: "<<distancia[destino]<<endl;
-    delete[] distancia;
-    delete[] antecessor;
+    if(verificados.count(destino)){
+        cout<<"Distancia do caminho minimo: "<<distancia[destino]<<endl;
+        Localizacao temp = destino;
+        while(true){
+            cout<<temp<<" ";
+            if(temp == inicio)
+                break;
+            temp = antecessor[temp];
+        }
+        cout<<"."<<endl;
+        return;
+    }
+
+    cout<<"Nao ha caminho de "<< inicio<< " ao "<< destino<< "."<<endl;
 }
 
 
@@ -119,20 +139,20 @@ int main()
     example_graph.arestas['A'] = {'B'};
     example_graph.arestas['B'] = {'A', 'C', 'D'};
     example_graph.arestas['C'] = {'A'};
-    example_graph.arestas['D'] = {'E', 'A'};
+    example_graph.arestas['D'] = {'A','E'};
     example_graph.arestas['E'] = {'B'};
 
     Matriz_Custo<char, unsigned int> matriz;
     matriz.arestas['A'] = {5};
     matriz.arestas['B'] = {3,3,2};
     matriz.arestas['C'] = {6};
-    matriz.arestas['D'] = {5,1};
+    matriz.arestas['D'] = {1,3};
     matriz.arestas['E'] = {7};
 
 
     busca_em_largura(example_graph, 'B');
-
-    caminho_minimo(example_graph, matriz,'A','A');
+    cout<<endl<<endl<<"Menor caminho!"<<endl;
+    dijkstra(example_graph, matriz,'C','E');
 
     return 0;
 }
